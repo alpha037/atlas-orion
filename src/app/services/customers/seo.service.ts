2f025f1f27d45b197de4f2f7767aefed674a3b44
alpha037 <subhronil.dutta@gmail.com>
2021-07-02 21:11:15 +0530

    Add: SEO service

---
+++ src/app/services/customers/seo.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeoService {
+  constructor(
+    private title: Title,
+    private meta: Meta,
+    private router: Router
+  ) {}
+
+  generateTags({ title = '', description = '', image = '' }): void {
+    this.title.setTitle(title);
+
+    this.meta.addTags([
+      // Open Graph Tags
+      {
+        name: 'og:url',
+        content: `https://orion.shubhranil.com${this.router.url}`,
+      },
+      { name: 'og:title', content: title },
+      { name: 'og:description', content: description },
+      { name: 'og:image', content: image },
+
+      // Twitter Card Tags
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@salpha037' },
+    ]);
+  }
+}
