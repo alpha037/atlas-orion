920ca6cf6b123a582faab24c17fb502ce69dd7ec
alpha037 <subhronil.dutta@gmail.com>
2021-07-02 21:13:27 +0530

    Mod: Update customer module providers

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,13 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { ListPageComponent } from './list-page/list-page.component';
 import { DetailPageComponent } from './detail-page/detail-page.component';
-
+import { CustomerService } from '../services/customers/customer.service';
+import { SeoService } from '../services/customers/seo.service';
 
 @NgModule({
   declarations: [ListPageComponent, DetailPageComponent],
-  imports: [
-    CommonModule,
-    CustomersRoutingModule
-  ]
+  imports: [CommonModule, CustomersRoutingModule],
+  providers: [CustomerService, SeoService],
 })
-export class CustomersModule { }
+export class CustomersModule {}
