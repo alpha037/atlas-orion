59e4109a74f362c049624158d37d99d51018de51
alpha037 <subhronil.dutta@gmail.com>
2021-07-10 22:43:59 +0530

    Mod: Update meta tags instead of adding them

--- src/app/services/customers/seo.service.ts
+++ src/app/services/customers/seo.service.ts
@@ -15,22 +15,43 @@ export class SeoService {
   generateTags({ title = '', description = '', image = '' }): void {
     this.title.setTitle(title);
 
-    this.meta.addTags([
-      { name: 'author', content: title },
-      { name: 'description', content: description },
+    // this.meta.addTags([
+    //   { name: 'author', content: title },
+    //   { name: 'description', content: description },
 
-      // Open Graph Tags
-      {
-        name: 'og:url',
-        content: `https://orion.shubhranil.com${this.router.url}`,
-      },
-      { name: 'og:title', content: title },
-      { name: 'og:description', content: description },
-      { name: 'og:image', content: image },
+    //   // Open Graph Tags
+    //   {
+    //     name: 'og:url',
+    //     content: `https://orion.shubhranil.com${this.router.url}`,
+    //   },
+    //   { name: 'og:title', content: title },
+    //   { name: 'og:description', content: description },
+    //   { name: 'og:image', content: image },
 
-      // Twitter Card Tags
-      { name: 'twitter:card', content: 'summary' },
-      { name: 'twitter:site', content: '@salpha037' },
-    ]);
+    //   // Twitter Card Tags
+    //   { name: 'twitter:card', content: 'summary' },
+    //   { name: 'twitter:site', content: '@salpha037' },
+    // ]);
+
+    this.meta.updateTag({ name: 'author', content: title });
+    this.meta.updateTag({ name: 'description', content: description });
+
+    // Open Graph Tags
+    this.meta.updateTag({ name: 'og:title', content: title });
+    this.meta.updateTag({ name: 'og:description', content: description });
+    this.meta.updateTag({ name: 'og:image', content: image });
+    this.meta.updateTag({
+      name: 'og:url',
+      content: `https://orion.shubhranil.com${this.router.url}`,
+    });
+
+    // Twitter Card Tags
+    this.meta.updateTag({ name: 'twitter:title', content: title });
+    this.meta.updateTag({ name: 'twitter:description', content: description });
+    this.meta.updateTag({ name: 'twitter:image', content: image });
+    this.meta.updateTag({
+      name: 'twitter:url',
+      content: `https://orion.shubhranil.com${this.router.url}`,
+    });
   }
 }
